209c7879c8d441b7c0b530f631ed785db342a634
Atilla-Koz <atillakoz@hotmail.com>
2024-05-21 21:56:50 +0300

    Done

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,19 +1,52 @@
 package org.example;
 
 public class Main {
-   public static  boolean isPalindrome(String str) {
-        if (str == null) {
-            return false;
+     public static boolean isPalindrome(int x) {
+
+          x = Math.abs(x);
+
+          int reversed = 0;
+          int original = x;
+
+          while (x != 0) {
+               reversed = reversed * 10 + x % 10;
+               x = x / 10;
+          }
+
+          return original == reversed;
+     }
+
+
+
+     public static boolean isPerfectNumber(int number) {
+          if (number < 1) {
+               return false;
+          }
+          int sum = 0;
+          for (int i = 1; i < number; i++) {
+               if (number % i == 0) {
+                    sum += i;
+               }
+          }
+          return sum == number;
+     }
+
+
+   public static String numberToWords(int number){
+        if(number < 0){
+             return "Invalid Value";
         }
-        int length = str.length();
-        for (int i = 0; i < length / 2; i++) {
-            if (str.charAt(i) != str.charAt(length - i - 1)) {
-                return false;
-            }
+        String[] words = {"Zero", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine"};
+        String result = "";
+        while(number != 0){
+             int digit = number % 10;
+             result = words[digit] + " " + result;
+             number = number / 10;
         }
-        return true;
-    }
-    public static void main(String[] args) {
-        System.out.println(isPalindrome("madam"));
-    }
+        return result.trim();
+   }
+
 }
+
+
+
